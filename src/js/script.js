6fb86cf896c0e88b68b0fd5637b3eb0c6d9e7462
Citrus <SandyFox17@yandex.ru>
2020-06-05 15:03:35 +0500

    modific slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        slidesToShow: 1,
+        adaptiveHeight: true,
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="btn_slide"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="btn_slide"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="btn_slide"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="btn_slide"></button>',
         responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                dots: true,
-                arrows:  false
-              }
+          {
+            breakpoint: 768,
+            settings: {
+              dots: true
             }
-          ]
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              dots: true
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
     });
   });
           
